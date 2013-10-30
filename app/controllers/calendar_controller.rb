6a87206cad4f2cc89aeb200921b79565df4b3b4a
Thinh Nguyen <thinhjb@gmail.com>
2013-10-30 08:44:15 -0700

    refreshable

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,10 @@
 class CalendarController < ApplicationController
+  respond_to :html, :js
   def index
-   @post = Event.new
+    @post = Event.new
+    respond_with(@post)
+  end
+  def show
   end
 
 end
